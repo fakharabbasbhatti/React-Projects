a3ceb8259fafb4fdf9e7dce52723a179a59ef956
Fakhar Abbas Bhatti <fakharabbasbhatti333@gmail.com>
2025-11-10 12:29:38 +0500

    start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h2 className='test-red-500 text-5xl bg-blue-500'>fakhar</h2>
+    </>
+  )
+}
+
+export default App
